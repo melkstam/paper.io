e4a01b207a9b3fd5db0a09a4373335fbb8ffba32
adamhalim <adaab301@student.liu.se>
2018-12-11 15:11:22 +0000

    Bots no longer can change into opposite directions

--- src/se/liu/ida/paperio/BotPlayer.java
+++ src/se/liu/ida/paperio/BotPlayer.java
@@ -5,17 +5,10 @@ import java.util.ArrayList;
 
 public class BotPlayer extends Player{
 
+    private double rand = Math.random();
     public BotPlayer(int height, int width){
         super(height, width);
-    }
 
-
-    /**
-     * Decides where the bot shall move and moves accordingly
-     */
-    @Override
-    public void move() {
-        double rand = Math.random();
         if (rand < 0.05) {
             dx = 1;
             dy = 0;
@@ -29,29 +22,82 @@ public class BotPlayer extends Player{
             dx = 0;
             dy = -1;
         }
+    }
+
+
+    /**
+     * Decides where the bot shall move and moves accordingly
+     */
+    @Override
+    public void move() {
+        double rand = Math.random();
+        if (rand < 0.05 && dx != -1) {
+            dx = 1;
+            dy = 0;
+        }else if (rand < 0.1 && dx != 1) {
+            dx = -1;
+            dy = 0;
+        }else if (rand < 0.15 && dy != -1) {
+            dx = 0;
+            dy = 1;
+        }else if (rand < 0.2 && dy != 1) {
+            dx = 0;
+            dy = -1;
+        }
         avoidOutOfBounds();
         x += dx;
         y += dy;
-
+        //avoidOutOfBounds();
     }
-    // TODO Make bots not go in the opposite direction (into their own trail)
     public void avoidOutOfBounds(){
-        if(x == 0){
-            dx = 1;
-            dy = 0;
-            System.out.println("Avoided Out of bounds");
-        }else if(x == width -1 ){
-            dx = -1;
-            dy = 0;
-            System.out.println("Avoided Out of bounds");
-        }else if(y == 0){
+        if(x == 0 && y == height - 1){
+            if(dx == -1){
+                dx = 0;
+                dy = -1;
+            }else {
+                dx = 1;
+                dy = 0;
+            }
+        }else if(x == width -1 && y == 0){
+            if(dx == 1){
+                dx = 0;
+                dy = 1;
+            } else {
+                dx = -1;
+                dy = 0;
+            }
+        }else if(x == width - 1 && y == height -1){
+            if(dx == 1){
+                dx = 0;
+                dy = -1;
+            }else {
+                dx = -1;
+                dy = 0;
+            }
+        }else if(x == 0 && y == 0){
+            if(dx == -1){
+                dx = 0;
+                dy = 1;
+            }else {
+                dx = 1;
+                dy = 0;
+            }
+        }else if(x == 0 && dx == -1){
             dx = 0;
             dy = 1;
-            System.out.println("Avoided Out of bounds");
-        }else if(y == height -1){
+            //System.out.println("Avoided Out of bounds");
+        }else if(x == width -1 &&  dx == 1){
             dx = 0;
-            dy = -1;
-            System.out.println("Avoided Out of bounds");
+            dy = 1;
+            //System.out.println("Avoided Out of bounds");
+        }else if(y == 0 && dy == -1){
+            dx = 1;
+            dy = 0;
+            //System.out.println("Avoided Out of bounds");
+        }else if(y == height -1 && dy == 1){
+            dx = 1;
+            dy = 0;
+            //System.out.println("Avoided Out of bounds");
         }
     }
 }
